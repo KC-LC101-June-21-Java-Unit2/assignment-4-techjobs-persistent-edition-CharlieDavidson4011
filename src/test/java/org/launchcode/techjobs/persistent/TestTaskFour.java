f1e9dc1b65793f5afdf6e90803b0da9f9dacc83e
Charlie Davidson <charliedavidson4011@gmail.com>
2021-11-30 15:41:45 -0600

    8080 Resolved

--- src/test/java/org/launchcode/techjobs/persistent/TestTaskFour.java
+++ src/test/java/org/launchcode/techjobs/persistent/TestTaskFour.java
@@ -17,6 +17,7 @@ import javax.persistence.ManyToMany;
 import java.io.IOException;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
